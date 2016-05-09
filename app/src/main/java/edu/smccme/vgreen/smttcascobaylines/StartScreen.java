f13d717891ec670c3d67fe59b41b37eb537ee19b
Skotbb <Skotbb@gmail.com>
2016-05-09 15:49:49 -0400

    Added a start screen, and changed the manifest.

---
+++ app/src/main/java/edu/smccme/vgreen/smttcascobaylines/StartScreen.java
@@ -0,0 +1,29 @@
+package edu.smccme.vgreen.smttcascobaylines;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartScreen extends AppCompatActivity {
+    private Button mStartSchedules;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+
+
+        mStartSchedules = (Button) findViewById(R.id.startSched_button);
+
+        mStartSchedules.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), FilterActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
